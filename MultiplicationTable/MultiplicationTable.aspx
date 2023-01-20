6d1f41ae44bc2577e0d58d91a07f83d887772d2d
Junior Morais <jrmorais3@gmail.com>
2023-01-19 21:18:24 -0300

    Add project files.

---
+++ MultiplicationTable/MultiplicationTable.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MultiplicationTable.aspx.cs" Inherits="MultiplicationTable.MultiplicationTable" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+    <h1>Tabuada</h1>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="dLista" runat="server" OnSelectedIndexChanged="dLista_SelectedIndexChanged">
+            </asp:DropDownList>
+            <asp:Button ID="btExecutar" runat="server" OnClick="btExecutar_Click" Text="Executar" />
+        </div>
+        <asp:ListBox ID="lbDados" runat="server" Height="247px" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged" Width="155px"></asp:ListBox>
+    </form>
+</body>
+</html>
---
+++ MultiplicationTable/MultiplicationTable.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Web.UI.WebControls;
+
+namespace MultiplicationTable
+{
+    public partial class MultiplicationTable : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Lista com os números disponíveis para a tabuada
+            // IsPostBack método de verificação se é uma página nova ou apenas atualizada. Previne a duplicação de informações no DropDownList
+            if (!IsPostBack)
+            {
+                for (int i = 1; i < 11; i++)
+                {
+                    dLista.Items.Add(i.ToString());
+                }
+            }
+
+        }
+        protected void btExecutar_Click(object sender, EventArgs e)
+        {
+            // limpa a ListBox para receber novos números
+            lbDados.Items.Clear();
+
+            // armazena o número selecionado na DropDownList
+            ListItem li = dLista.SelectedItem;
+            // converte essa string para Int32
+            var n = Convert.ToInt32(li.Value);
+
+            // faz a adição dos itens e retorna na tela
+            for (int i = 1; i < 11; i++)
+            {
+                lbDados.Items.Add($"{i.ToString()} x {n.ToString()} = {i*n}");
+            }
+        }
+
+        protected void dLista_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
